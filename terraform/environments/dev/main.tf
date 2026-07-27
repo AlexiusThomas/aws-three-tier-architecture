1eb4d0d5beaf3e49c65071a248a2ec26cd20f682
Alexius Thomas <alexiusvthomas@gmail.com>
2026-07-27 17:36:22 -0400

    Add Terraform validation workflow

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -22,4 +22,63 @@ provider "aws" {
   }
 }
 
+module "vpc" {
+  source = "../../modules/vpc"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  vpc_cidr                  = var.vpc_cidr
+  public_subnet_1_cidr      = var.public_subnet_1_cidr
+  public_subnet_2_cidr      = var.public_subnet_2_cidr
+  private_app_subnet_1_cidr = var.private_app_subnet_1_cidr
+  private_app_subnet_2_cidr = var.private_app_subnet_2_cidr
+  private_db_subnet_1_cidr  = var.private_db_subnet_1_cidr
+  private_db_subnet_2_cidr  = var.private_db_subnet_2_cidr
+}
+
+module "security" {
+  source = "../../modules/security"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  vpc_id = module.vpc.vpc_id
+}
+
 # Add project-specific modules and resources below.
+module "alb" {
+  source = "../../modules/alb"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  vpc_id                = module.vpc.vpc_id
+  public_subnet_ids     = module.vpc.public_subnet_ids
+  alb_security_group_id = module.security.alb_security_group_id
+  enabled               = false
+}
+
+module "compute" {
+  source = "../../modules/compute"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  private_app_subnet_ids = module.vpc.private_app_subnet_ids
+  app_security_group_id  = module.security.app_security_group_id
+
+  enabled = false
+}
+
+module "rds" {
+  source = "../../modules/rds"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  private_db_subnet_ids      = module.vpc.private_db_subnet_ids
+  database_security_group_id = module.security.database_security_group_id
+
+  enabled = false
+}s
